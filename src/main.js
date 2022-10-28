2f32b5b8fea1f49340b3ccce215d1c26f686e781
Kaleab Alemayehu <cipherlofi04@gmail.com>
2022-10-28 23:41:12 +0300

    removing getType utility and get type form types eventHandlers

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import Search from './models/Search';
 import { dom } from './domStrings';
 
-const getType = () => {
-  // change HTML Collection into array to make it iterable
-  const types = Array.from(dom.type);
-  // iterate over the the list to find selected class
-  for (let i = 0; i < types.length; i++) {
-    const classTokens = Array.from(types[i].classList);
-    if (classTokens.includes('selected')) {
-      // geting data-* attribute from the list
-      return types[i].dataset.id;
-    }
-  }
+// change HTML Collection into array to make it iterable
+const types = Array.from(dom.type);
+const state = {
+  type: '',
+  number: '',
 };
 
 const randomHandler = async (e) => {
@@ -22,4 +16,18 @@ const randomHandler = async (e) => {
 
 ['click', 'touchstart'].forEach((e) => {
   dom.random.addEventListener(e, randomHandler);
+});
+// type conntroller
+
+const typeHandler = (e) => {
+  for (let i = 0; i < types.length; i++) {
+    types[i].classList.remove('selected');
+  }
+  e.target.classList.toggle('selected');
+  state.type = e.target.dataset.id;
+};
+types.forEach((type) => {
+  ['click', 'touchstart'].forEach((e) => {
+    type.addEventListener(e, typeHandler);
+  });
 });
